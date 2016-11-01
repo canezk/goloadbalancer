d0e5e3040724d8eb957499d3bba0f373647a8049
zhoukang <zhoukang199191@126.com>
2016-11-01 14:19:50 +0800

    MOD:增加http转发到指定server的逻辑

--- src/golangloadbalancer/consistenthash/consistenthash.go
+++ src/golangloadbalancer/consistenthash/consistenthash.go
@@ -2,3 +2,12 @@
 一致性哈希算法
  */
 package consistenthash
+
+import (
+	"log"
+)
+
+func GetCurrentRequestServer() (server string)  {
+	log.Println("Test!")
+	return
+}
--- src/golangloadbalancer/server/netconnector.go
+++ src/golangloadbalancer/server/netconnector.go
@@ -1,1 +1,36 @@
+/**
+包括如下：
+1、http server listner
+ */
 package server
+
+import (
+	"net/http"
+	"log"
+	"golangloadbalancer/consistenthash"
+)
+
+func Listen(port int)  {
+	http.Handle("/", http.HandlerFunc(balanceHandler))
+	log.Fatal(http.ListenAndServe(":" + port, nil))
+}
+
+func balanceHandler(w http.ResponseWriter, req *http.Request)  {
+	url := req.URL
+	url.Host = consistenthash.GetCurrentRequestServer()
+
+	proxyReq, err := http.NewRequest(req.Method, url.String(), req.Body)
+	if err != nil {
+		log.Fatal("Proxy error!", err)
+	}
+	proxyReq.Header = make(http.Header)
+	for h, val := range req.Header {
+		proxyReq.Header[h] = val
+	}
+	client := &http.Client{}
+	resp, err := client.Do(proxyReq)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadGateway)
+	}
+	defer resp.Body.Close()
+}
